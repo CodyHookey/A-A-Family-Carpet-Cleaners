7769727d809f78f5208a4b7b1927796328608d20
Cody Hookey <149050408+CodyHookey@users.noreply.github.com>
2026-03-12 17:46:18 +0000

    Improving Hero Image Optimisation

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -42,6 +42,8 @@ function HeroSection() {
             alt=""
             className="img-fluid rounded hero-image"
             width={"450px"}
+            fetchpriority="high"
+            decoding="async"
           />
         </div>
       </div>
